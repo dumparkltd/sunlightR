dcaf45d2bf9c5ffe8313bcdb60dcd29a6fec7c17
tmfrnz <timo@dumpark.com>
2023-01-14 17:02:26 +0100

    calculate horizon for possible azimuths

--- scripts/get_horizon.R
+++ scripts/get_horizon.R
@@ -15,10 +15,59 @@
 # Dependencies
 # - get_min_altitude_for_azimuth
 #
+library(suncalc)
+source("scripts/get_min_altitude_for_azimuth.R")
+source("scripts/rad2deg.R")
 
-get_horizon <- function (lat, lon, dem, res_azimuths) {
-  # TODO
+get_horizon <- function (lon, lat, dem, res_azimuths, d_transect) {
+  date_longest_N = as.Date("2023-06-21")
+  date_longest_S = as.Date("2023-12-21")
+  # get azimuth boundaries
+  if (lat > 0) {
+    sltimes <- getSunlightTimes(
+      date = date_longest_N,
+      lat = lat,
+      lon = lon,
+      keep = c("sunrise", "sunset"),
+      tz = "UTC"
+    )
+  } else {
+    sltimes <- getSunlightTimes(
+      date = date_longest_S,
+      lat = lat,
+      lon = lon,
+      keep = c("sunrise", "sunset"),
+      tz = "UTC"
+    )
+  }
+  # in radians 
+  # "sun azimuth in radians (direction along the horizon, measured 
+  # from south to west), e.g. 0 is south and Math.PI * 3/4 is northwest"
+  slp_sunrise = getSunlightPosition(
+    date= sltimes$sunrise,
+    lat = lat,
+    lon = lon,
+    keep = c( "azimuth")
+  )
+  slp_sunset = getSunlightPosition(
+    date= sltimes$sunset,
+    lat = lat,
+    lon = lon,
+    keep = c( "azimuth")
+  )
+  # convert to degrees, North to East, round up/down
+  # WARNING should only work on N hemisphere
+  azimuth_min = floor(rad2deg(slp_sunrise$azimuth)+180)
+  azimuth_max = ceiling(rad2deg(slp_sunset$azimuth)+180)
   # get all minimum azimuths (from 0 to 360)
-  
-  return(horizon)
+  altitudes <- c()
+  azimuths <- c()
+  for (i in seq(azimuth_min, azimuth_max, by = res_azimuths)) {
+    altitudes = append(altitudes, get_min_altitude_for_azimuth(lon, lat, i, dem, d_transect))
+    azimuths = append(azimuths, i)
+  }
+  return (data.frame(
+    azimuths = azimuths,
+    altitudes = altitudes
+  ))
 }
--- scripts/main.R
+++ scripts/main.R
@@ -1,21 +1,30 @@
 library(sp)
 library(raster)
 
+source("scripts/get_horizon.R")
 source("scripts/get_min_altitude_for_azimuth.R")
+source("scripts/get_transect.R")
 
 # load raster
 dem = raster("~/projects/INRAE/data/raster1.tif")
 
-# angle of the sun
-azimuth = 135
-
-#lon = 0.818621526725745
-#lat = 42.841380715123769
-lon = 0.803670577449159
-lat = 42.867089743756523
+# test location
+lon = 0.818621526725745
+lat = 42.841380715123769
+# lon = 0.803670577449159
+# lat = 42.867089743756523
 
 # the distance between elevation points (in m)
-d_transect = 100
+d_transect = 10
 
-min_altitude = get_min_altitude_for_azimuth(lon, lat, azimuth, dem, d_transect)
+# horizontal resolution of azimuth (in degrees)
+resolution_azimuths = 2 
 
+horizon = get_horizon(lon, lat, dem, resolution_azimuths, d_transect)
+
+plot(horizon)
+
+# azimuth = 210
+# transect = get_transect(lon, lat, azimuth, dem, d_transect)
+# 
+# get_min_altitude_for_azimuth(lon, lat, azimuth, dem, d_transect)
